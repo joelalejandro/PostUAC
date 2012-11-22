904372caf0c21ebe49cdf3dc061841c08c71ae62
Joel A. Villarreal Bertoldi <joel.alejandro@moobin.net>
2012-11-22 17:33:20 -0300

    Versión 0.3. Excluye las páginas no permitidas de un menú generado automáticamente por Wordpress. Sigue pendiente la exclusión de items vinculados por menús customizados.

--- wp-content/plugins/PostUAC/PostUAC.php
+++ wp-content/plugins/PostUAC/PostUAC.php
@@ -3,13 +3,14 @@
 Plugin Name: PostUAC
 Plugin URI: http://moobin.net
 Description: User Access Control at the post level.
-Version: 0.2
+Version: 0.3
 Author: Joel A. Villarreal Bertoldi (at Moobin)
 Author URI: http://moobin.net
 License: MIT
 */
 
 $postuac_plugin_url = plugin_dir_url(__FILE__);
+$postuac_user_allowed_pages = array();
 
 function postuac_get_users_list() {
   $users = array();
@@ -20,14 +21,13 @@ function postuac_get_users_list() {
 };
 
 function postuac_post_meta_box() {
-  add_meta_box( "postuac-metabox", "Usuarios permitidos", "postuac_post_meta_box_form", "page", "side", "high");
-  add_meta_box( "postuac-metabox", "Usuarios permitidos", "postuac_post_meta_box_form", "post", "side", "high");
+  add_meta_box("postuac-metabox", "Usuarios permitidos", "postuac_post_meta_box_form", "page", "side", "high");
+  add_meta_box("postuac-metabox", "Usuarios permitidos", "postuac_post_meta_box_form", "post", "side", "high");
 };
 
 function postuac_post_meta_box_form() {
-  $is_public = get_post_meta($_REQUEST["post"], "postuac-is-public", true);
-
-  $allowed_users = get_post_meta($_REQUEST["post"], "postuac-users", false);
+  $is_public = postuac_is_post_public($_REQUEST["post"]);
+  $allowed_users = postuac_get_allowed_users($_REQUEST["post"]);
   $security_token = wp_create_nonce("pre_publish_validation");
 
   include "Widget.php";
@@ -42,21 +42,36 @@ function postuac_save_post($postID) {
   };
 };
 
-function postuac_is_user_allowed_on_this_post($wp_query) {
-  $allowed_users = get_post_meta($wp_query->post->ID, "postuac-users", false);
-  $is_public = get_post_meta($wp_query->post->ID, "postuac-is-public", true);
+function postuac_get_allowed_users($postID) {
+  return get_post_meta($postID, "postuac-users", false);
+};
 
-  $redirect_url = "/";
+function postuac_is_post_public($postID) {
+  return get_post_meta($postID, "postuac-is-public", true);
+};
+
+function postuac_is_allowed($postID) {
+  $is_public = postuac_is_post_public($postID);
+  $allowed_users = postuac_get_allowed_users($postID);
   $user = wp_get_current_user();
 
-  if ($is_public == "") $is_public = "yes";
-  if ($is_public == "yes") return;
+  if ($is_public == "" || $is_public == "yes") { return true; }
+  if ($user->user_nicename == "admin") { return true; }
 
-  if ($user->ID != 0 && $user->user_nicename != "admin" && !in_array($user->ID, $allowed_users)) {
+  if ($user->ID == 0) { return false; }
+  if (!in_array($user->ID, $allowed_users)) { return false; }
+
+  return false;
+};
+
+function postuac_is_user_allowed_on_this_post($wp_query) {
+  $redirect_url = "/";
+
+  if (!postuac_is_allowed($wp_query->post->ID)) {
     echo "<div class='post not allowed' data-post-id='{$wp_query->post->ID}' style='display:none'></div>";
   };
 
-  if ($wp_query->is_singular == 1 && $user->ID != 0 && $user->user_nicename != "admin" && !in_array($user->ID, $allowed_users)) {
+  if ($wp_query->is_singular == 1 && !postuac_is_allowed($wp_query->post->ID)) {
     echo "<script type=\"text/javascript\">location = \"$redirect_url\";</script>";
     die;
   };
@@ -76,8 +91,39 @@ function postuac_validate_post_script($page) {
   };
 };
 
+function postuac_get_allowed_pages() {
+  global $postuac_user_allowed_pages;
+  $user = wp_get_current_user();
+  $pages = get_pages();
+  $excluded = array();
+  foreach ($pages as $page) {
+    if (get_post_meta($page->ID, "postuac-is-public", true) == "yes") continue;
+    if (!in_array($user->ID, get_post_meta($page->ID, "postuac-users"))) {
+      $excluded[] = $page->ID;
+    };
+  };
+  $query = "echo=0&title_li=&exclude=" . implode(",", $excluded);
+  $postuac_user_allowed_pages = wp_list_pages($query);
+
+  print_r(wp_get_nav_menus());
+
+  add_filter("wp_list_pages", "postuac_delete_forbidden_nav_menu_items");
+}
+
+function postuac_delete_forbidden_nav_menu_items($items, $args) {
+  global $postuac_user_allowed_pages;
+  $user = wp_get_current_user();
+  
+  if ($user->user_nicename == "admin") return $items;
+
+  return $postuac_user_allowed_pages;
+};
+
+add_action("wp_head", "postuac_get_allowed_pages");
 add_action("loop_start", "postuac_is_user_allowed_on_this_post");
 add_action("admin_menu", "postuac_post_meta_box");
 add_action("save_post", "postuac_save_post", 10, 1);
 add_action("admin_enqueue_scripts", "postuac_validate_post_script");
 add_action("wp_footer", "postuac_delete_forbidden_posts_script");
+
+add_filter("wp_nav_menu_items", "postuac_delete_forbidden_nav_menu_items", null, 3);
